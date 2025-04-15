82925f0d77291b62176a674d75e1edea61e04ca3
larissavitoria06 <larissavitoria.ssntos12@gmail.com>
2025-04-15 15:49:20 -0300

    prisma

--- api/src/routes.js
+++ api/src/routes.js
@@ -5,4 +5,25 @@ routes.get('/', (req, res) => {
   return res.json({ titulo: 'Biblioteca ACME' });
 });
 
+const Aluno = require('./controller/Alunos');
+const Emprestimos = require('./controller/emprestimos');
+const Livros = require('./controller/livros');
+
+router.post('/alunos',Alunos.create);
+router.get('/alunos',Alunos.read);
+router.get('/alunos/:id',Alunos.readOne);
+router.patch('/alunos/:id',Alunos.update);
+router.delete('/alunos/:id',Alunos.remove);
+
+router.post('/emprestimos',Emprestimos.create);
+router.get('/emprestimos',Emprestimos.read);
+router.patch('/emprestimos/:id',Emprestimos.update);
+router.delete('/emprestimos/:id',Emprestimos.remove);
+
+router.post('/livros',Livros.create);
+router.get('/livros',Livros.read);
+router.get('/livros/:id',Livros.readOne);
+router.patch('/livros/:id',Livros.update);
+router.delete('/livros/:id',Livros.remove);
+
 module.exports = routes;
